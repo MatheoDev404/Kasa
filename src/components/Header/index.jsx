b93fa57e41c584fa9ae41664c3099cc1f811e327
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-03-04 16:13:10 +0100

    Ajout
     - Styled-comonent => node_modules
     - utils/style/color.js => ajout primary color

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.svg'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: ${colors.primary};
+  text-decoration: none;
+  font-size: 18px;
+`
 
 function Header() {
   return (
     <header>
       <img src={logo} className="Home-logo" alt="logo" />
       <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/About" $isFullLink>
-          A propos
-        </Link>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/About">A propos</StyledLink>
       </nav>
     </header>
   )
